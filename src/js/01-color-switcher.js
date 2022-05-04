9e689c13559ed7a3cb76b79b428d8615855c097e
unknown <90862670+Vadim-kaR@users.noreply.github.com>
2022-05-04 12:46:22 +0300

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,31 @@
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body')
+};
 
-// console.log('hello')
+let timer = null;
+
+refs.btnStop.disabled = true;
+
+refs.btnStart.addEventListener('click', onStartDisco);
+refs.btnStop.addEventListener('click', onStopDisco);
+
+function onStartDisco() { 
+    timer = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    refs.btnStop.disabled = false;
+    refs.btnStart.disabled = true;
+}
+
+function onStopDisco() { 
+    clearInterval(timer);
+    refs.btnStop.disabled = true;
+    refs.btnStart.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
