c38b96d9d0bd33075524e03e4ec335fd79c68fd6
unknown <90862670+Vadim-kaR@users.noreply.github.com>
2022-05-07 20:47:31 +0300

    task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const refs = {
+  form:document.querySelector('.form')
+}
+
+const formData = {};
+
+refs.form.addEventListener('input', e => {
+  formData[e.target.name] = e.target.value;
+});
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) { 
+  e.preventDefault();
+  for (let i = 0; i < formData.amount; i += 1) { 
+
+    createPromise(formData.step, formData.delay)
+    .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
   }
+
+  refs.form.reset()
+}
+
+function createPromise(position, delay) {
+
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+
+    setTimeout(() => { 
+      if (shouldResolve) {
+        resolve({ position, delay } );
+        } else {
+        reject({ position, delay });
+        }
+
+    }, delay)
+  });
 }
